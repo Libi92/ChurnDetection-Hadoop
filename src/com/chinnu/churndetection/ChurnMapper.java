b9282da0eca6f4d1220130ea0e3b58f81fd7a8c5
geojit <geojit@Jayadevs-MacBook-Pro.local>
2017-04-21 18:07:30 +0530

    line split

--- src/com/chinnu/churndetection/ChurnMapper.java
+++ src/com/chinnu/churndetection/ChurnMapper.java
@@ -13,26 +13,43 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class ChurnMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	static String BASEURL = "/user/libin/ChurnDetection/";
-    static String INPUT = BASEURL + "raw/input.csv";
+	private static String BASEURL = "/user/libin/ChurnDetection/";
+    private static String INPUT = BASEURL + "raw/data.csv";
+    
+    private static int DAY_CALL_INDEX = 9;
+    private static int EVE_CALL_INDEX = 12;
+    private static int NIGHT_CALL_INDEX = 15;
+    private static int INTR_CALL_INDEX = 18;
     
 	public void map(LongWritable ikey, Text ivalue, Context context) throws IOException, InterruptedException {
 		String line = ivalue.toString();
-		String[] words = line.split(",");
+		String[] concepts = line.split(",");
+		int dayCalls = Integer.parseInt(concepts[DAY_CALL_INDEX]);
+		int eveCalls = Integer.parseInt(concepts[EVE_CALL_INDEX]);
+		int nightCalls = Integer.parseInt(concepts[NIGHT_CALL_INDEX]);
+		int intrCalls = Integer.parseInt(concepts[INTR_CALL_INDEX]);
+		
+		Configuration conf = new Configuration();
+		FileSystem fileSystem = FileSystem.get(conf);
+		Path inputPath = new Path(INPUT);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileSystem.open(inputPath)));
+		String inLine;
+		
+		while ((inLine = bufferedReader.readLine()) != null) {
+			String[] split = inLine.split(",");
+			
+			int xDayCalls = Integer.parseInt(split[DAY_CALL_INDEX]);
+			int xEveCalls = Integer.parseInt(split[EVE_CALL_INDEX]);
+			int xNightCalls = Integer.parseInt(split[NIGHT_CALL_INDEX]);
+			int xIntrCalls = Integer.parseInt(split[INTR_CALL_INDEX]);
+		}
+		
 		for (String word : words) {
 			Text outputKey = new Text(word.toUpperCase().trim());
 			IntWritable outputValue = new IntWritable(1);
 			context.write(outputKey, outputValue);
 			
-			Configuration conf = new Configuration();
-			FileSystem fileSystem = FileSystem.get(conf);
-			Path inputPath = new Path(INPUT);
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileSystem.open(inputPath)));
-			String inLine;
 			
-			while ((inLine = bufferedReader.readLine()) != null) {
-				
-			}
 		}
 	}
 
